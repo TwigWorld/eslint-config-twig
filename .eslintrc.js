6a0c75586c5a2afd3b47db1d079194b096bacd23
Adam Oliver <mail@adamoliver.net>
2020-11-27 15:34:19 +0000

    Update to latest Eslint config from AirBnb

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,35 @@
 module.exports = {
-  extends: ["airbnb", "plugin:react/recommended"],
-  parser: "babel-eslint",
-  env: {
-    browser: true,
-    node: true,
-    jest: true
-  },
-  rules: {
-    "indent": [2, 4], // 4 spaces for indentation
-    "react/jsx-indent": [2, 4], // 4 spaces for indentation
-    "react/jsx-indent-props": [2, 4], // 4 spaces for indentation
-    "max-len": [2, 120], // 120 max line length
-    "class-methods-use-this": 0, // Needed to return JSX from React component methods
-    "compat/compat": 2,
-    "function-paren-newline": ["error", "consistent"],
-    "no-underscore-dangle": ["error", { "allow": ["__typename"] }],
-    "no-process-env": "error", // Use getEnv instead
-    "jsx-a11y/anchor-is-valid": ["error", {
-        "components": ["Link"],
-        "specialLink": ["to"],
-        "aspects": ["noHref"]
-    }],
-    "jsx-a11y/label-has-for": "off", // Deprecated and broken in eslint 6.1.0 Airbnb uses label- has - associated - control instead
-    "operator-linebreak": ["error", "after"],
-    "no-only-tests/no-only-tests": "error" // Do not allow .only in test files
-  },
-  plugins: [
-    "compat",
-    "no-only-tests"
-  ]
+    extends: ['airbnb', 'plugin:react/recommended'],
+    parser: 'babel-eslint',
+    env: {
+        browser: true,
+        node: true,
+        jest: true,
+    },
+    rules: {
+        indent: [2, 4], // 4 spaces for indentation
+        'react/jsx-indent': [2, 4], // 4 spaces for indentation
+        'react/jsx-indent-props': [2, 4], // 4 spaces for indentation
+        'max-len': [2, 120], // 120 max line length
+        'class-methods-use-this': 0, // Needed to return JSX from React component methods
+        'compat/compat': 2,
+        'function-paren-newline': ['error', 'consistent'],
+        'no-underscore-dangle': ['error', { allow: ['__typename'] }],
+        'no-process-env': 'error', // Use getEnv instead
+        'jsx-a11y/anchor-is-valid': [
+            'error',
+            {
+                components: ['Link'],
+                specialLink: ['to'],
+                aspects: ['noHref'],
+            },
+        ],
+        'operator-linebreak': ['error', 'after'],
+        'no-only-tests/no-only-tests': 'error',
+        'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    },
+    plugins: [
+        'compat',
+        'no-only-tests',
+    ],
 };
